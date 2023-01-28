0c522c811eb5e0011fe2d148ae27cccb557c26f6
DDanielson24 <daniel162400@gmail.com>
2023-01-28 13:39:20 +0100

    pull Repo hecho

--- Trafico/TraficoProducer/src/main/java/org/TFGInformatica/TraficoProducer.java
+++ Trafico/TraficoProducer/src/main/java/org/TFGInformatica/TraficoProducer.java
@@ -1,10 +1,25 @@
 package org.TFGInformatica;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TraficoProducer {
 
     public static void main(String[] args) {
 
         //1. Hacer git pull al repositorio remoto que contiene el último archivo a procesar
+        try {
+            ProcessBuilder pb = new ProcessBuilder("/home/daniel/Escritorio/TFGInformatica/pullRepo.sh");
+            Process p = pb.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
 
