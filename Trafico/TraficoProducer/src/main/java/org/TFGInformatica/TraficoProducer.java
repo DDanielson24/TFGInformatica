e04a581b1ce54b17a380240be042be597adf325f
DDanielson24 <daniel162400@gmail.com>
2023-01-28 13:27:05 +0100

    script de shell para actualizar el repo

--- Trafico/TraficoProducer/src/main/java/org/TFGInformatica/TraficoProducer.java
+++ Trafico/TraficoProducer/src/main/java/org/TFGInformatica/TraficoProducer.java
@@ -1,7 +1,18 @@
 package org.TFGInformatica;
 
 public class TraficoProducer {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1. Hacer git pull al repositorio remoto que contiene el último archivo a procesar
+
+
+
+
+        //2. Leer el archivo realizando las transformaciones necesarias
+
+        //3. Crear el productor de Kafka y enviar a través del topic
+
+
     }
 }
