d41751875a56bb6f2beebd18d52f5532d6f0b3ca
DDanielson24 <daniel162400@gmail.com>
2023-01-31 20:34:02 +0100

    creación clase EstacionDeServicio con avro

--- Gasolina/GasolinaProducer/src/main/java/org/TFGInformatica/GasolinaProducer.java
+++ Gasolina/GasolinaProducer/src/main/java/org/TFGInformatica/GasolinaProducer.java
@@ -1,4 +1,37 @@
 package org.TFGInformatica;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class GasolinaProducer {
+
+    public static void main(String[] args) {
+
+        try {
+            URL url = new URL("https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/");
+
+            HttpURLConnection restConnection = (HttpURLConnection) url.openConnection();
+            restConnection.setRequestMethod("GET");
+            restConnection.connect();
+
+            int responseCode = restConnection.getResponseCode();
+
+            if (responseCode != 200) {
+                throw new RuntimeException("HTTPResponseCode: " + responseCode);
+            }
+            else {
+
+
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
