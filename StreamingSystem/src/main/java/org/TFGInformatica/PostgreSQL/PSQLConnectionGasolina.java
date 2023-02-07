8edf1c5daca02406a96fa6dc73dce8dfd71d8af5
DDanielson24 <daniel162400@gmail.com>
2023-02-07 13:31:38 +0100

    inicio creación clases conexión PostgreSQL

---
+++ StreamingSystem/src/main/java/org/TFGInformatica/PostgreSQL/PSQLConnectionGasolina.java
@@ -0,0 +1,31 @@
+package org.TFGInformatica.PostgreSQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PSQLConnectionGasolina {
+
+    private final String HOST = "localhost";
+    private final String PUERTO = "5432";
+    private final String DATABASE = "GasolinaData";
+    private final String USER = "fdaniel";
+    private final String PASSWORD = "TFGInformatica";
+    private String url = "jdbc:postgresql://" + HOST + ":" + PUERTO + "/" + DATABASE;
+    private Connection conn = null;
+
+    public Connection connect() {
+        try {
+
+            conn = DriverManager.getConnection(url, USER, PASSWORD);
+            System.out.println("Se ha conectado exitosamente a la BD: " + DATABASE);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return conn;
+
+    }
+
+}
