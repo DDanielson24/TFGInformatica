4b390145e226900527b0cb2ba1480cfb2190d8b4
DDanielson24 <94745835+DDanielson24@users.noreply.github.com>
2023-02-20 22:25:24 +0100

    actualización shell script descarga ficheros

--- StreamingSystem/data/data.sh
+++ StreamingSystem/data/data.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-cd './Data/Trafico'
-pwd
+# Nos colocamos en el directorio adecuado
+cd './StreamingSystem/data'
+
+# Descargamos el fichero de tráfico
 wget 'https://datos.madrid.es/egob/catalogo/202087-0-trafico-intensidad.xml' -O 'ficheroTrafico.xml'
+echo "Fichero tráfico descargado exitosamente"
+
+# Descargamos el fichero de gasolina
+wget 'https://geoportalgasolineras.es/resources/files/preciosEESS_es.xls' -O 'ficheroGasolina.xls'
+echo "Fichero gasolina descargado exitosamente"
