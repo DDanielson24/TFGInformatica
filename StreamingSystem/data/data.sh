7beec8e0e1a15afc501b6ee6fbd7ecde18ad8436
DDanielson24 <daniel162400@gmail.com>
2023-02-22 20:29:55 +0100

    eliminación parte github actions

--- StreamingSystem/data/data.sh
+++ StreamingSystem/data/data.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Nos colocamos en el directorio adecuado
-cd './StreamingSystem/data'
-
 # Descargamos el fichero de tráfico
 wget 'https://datos.madrid.es/egob/catalogo/202087-0-trafico-intensidad.xml' -O 'ficheroTrafico.xml'
 echo "Fichero tráfico descargado exitosamente"
 
 # Descargamos el fichero de gasolina
-curl -o 'ficheroGasolina.xls' 'https://geoportalgasolineras.es/resources/files/preciosEESS_es.xls'
+wget -o 'ficheroGasolina.xls' 'https://geoportalgasolineras.es/resources/files/preciosEESS_es.xls'
 echo "Fichero gasolina descargado exitosamente"
