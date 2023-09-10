6c492591f79919480480ac142ba5567fbfce0f32
daniel <daniel162400@gmail.com>
2023-09-10 21:04:38 +0200

    corregido comentario en kafkaInit.sh

--- kafkaInit.sh
+++ kafkaInit.sh
@@ -2,5 +2,9 @@
 #
 # 1. Zookeeper
 /home/daniel/confluent-7.3.2/bin/zookeeper-server-start /home/daniel/confluent-7.3.2/etc/kafka/zookeeper.properties & 
+
+# 2. Kafka Server
 /home/daniel/confluent-7.3.2/bin/kafka-server-start /home/daniel/confluent-7.3.2/etc/kafka/server.properties &
+
+# 3. Schema Registry
 /home/daniel/confluent-7.3.2/bin/schema-registry-start /home/daniel/confluent-7.3.2/etc/schema-registry/schema-registry.properties
