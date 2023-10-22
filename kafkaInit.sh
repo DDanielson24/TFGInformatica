c463ca02694f83debf9e8d01c9a9f0785d84d968
daniel <daniel162400@gmail.com>
2023-10-22 17:27:25 +0200

    añadido a KafkaInit la inicialización de los 3 brokers

--- kafkaInit.sh
+++ kafkaInit.sh
@@ -4,7 +4,10 @@
 /home/daniel/confluent-7.3.2/bin/zookeeper-server-start /home/daniel/confluent-7.3.2/etc/kafka/zookeeper.properties & 
 
 # 2. Kafka Server
-/home/daniel/confluent-7.3.2/bin/kafka-server-start /home/daniel/confluent-7.3.2/etc/kafka/server.properties &
+#Broker 0
+/home/daniel/confluent-7.3.2/bin/kafka-server-start /home/daniel/confluent-7.3.2/etc/kafka/server.broker0.properties &
+/home/daniel/confluent-7.3.2/bin/kafka-server-start /home/daniel/confluent-7.3.2/etc/kafka/server.broker1.properties &
+/home/daniel/confluent-7.3.2/bin/kafka-server-start /home/daniel/confluent-7.3.2/etc/kafka/server.broker2.properties &
 
 # 3. Schema Registry
 /home/daniel/confluent-7.3.2/bin/schema-registry-start /home/daniel/confluent-7.3.2/etc/schema-registry/schema-registry.properties
